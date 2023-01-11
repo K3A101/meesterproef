85ec9f30ec4435dcfebad4173adab742375f45f0
K3A101 <keisha.alexander@hva.nl>
2023-01-11 13:14:56 +0100

    Preloader toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,41 @@
 console.log("Hello world");
 
-// const afbeeldingen = [
-// 'url("../images/new-york-city.png")', 'url("../images/medieval-times.png")', 'url("../images/mermaid-shopping-1.png")', 'url("../images/mermaid-shopping-2.png")',
-//  'url("../images/old-city.png")', 'url("../images/old-nyc.png")', 'url("../images/superwoman-comic.png")',
-// 'url("../images/sushi-head.png")', 'url("../images/sushi-with-fork.png")']
+var loader;
 
-// const afbeeldingContainer = document.getElementById("image");
-// var button = document.querySelector("button");
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function () {
+            loadNow(opacity - 0.05);
+        }, 80);
+    }
+}
 
-// button.addEventListener('click', (evt) => {
-//     var a = Math.floor(Math.random() * afbeeldingen.length);
-//     var img = afbeeldingen[a];
-//     afbeeldingContainer.style.backgroundImage = img;
-// });
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
+// https://nishanc.medium.com/how-to-add-a-gif-pre-loader-to-your-website-dcfd29bd055d
 
 const afbeeldingen = [{
-        img: "../images/new-york-city.png",
+        img: "./images/new-york-city.png",
         text: "A neoclassicism oil painting of new york",
     },
 
     {
-        img: "../images/medieval-times.png",
+        img: "./images/medieval-times.png",
         text: "A Medieval fantasy world with dragons and Castle",
     },
 
     {
-        img: "../images/mermaid-shopping-1.png",
+        img: "./images/mermaid-shopping-1.png",
         text: "A digital art of a mermaid shopping in new york city during the night with alot of stars",
     },
     {
@@ -33,23 +43,23 @@ const afbeeldingen = [{
         text: "A realism painting of a mermaid shopping in new york city during the night with alot of stars",
     },
     {
-        img: "../images/old-city.png",
+        img: "./images/old-city.png",
         text: "A neoclassism oil painting of a city",
     },
     {
-        img: "../images/old-nyc.png",
+        img: "./images/old-nyc.png",
         text: "A neoclassism oil painting of a city",
     },
-    {
-        img: "../images/superwoman-comic.png",
+    { 
+        img: "./images/superwoman-comic.png",
         text: "A woman in a super hero comstume flying through the sky, pop art",
     },
     {
-        img: "../images/sushi-head.png",
+        img: "./images/sushi-head.png",
         text: "An sushi head holding a fork standing on a bed, neoclassism painting",
     },
     {
-        img: "../images/sushi-with-fork.png",
+        img: "./images/sushi-with-fork.png",
         text: "An sushi head holding a fork standing on a bed, neoclassism painting",
     }
 
@@ -77,3 +87,5 @@ var robotAnimation = bodymovin.loadAnimation({
     autoplay: true,
     path: '../animaties/robot-karakter-animation.json'
 })
+
+
